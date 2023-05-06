c0c3c7ac2443a7c4eb553574426797502cc2a256
siobopfadzani <pfadzani@gmail.com>
2023-05-06 15:47:31 +0200

    commit

---
+++ fundamentals/mycode/string test1.java
@@ -0,0 +1,11 @@
+
+public class StringTest1{
+    public static void main(String [] args){
+        String name = "pfadzani";
+        String surname = "siobo";
+		//String fullName = name + surname;
+
+        System.out.println("Full Name :"+name+" "+surname);
+    }
+}
+

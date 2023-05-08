a244e49356372167b4f043d0cc17b84ae280730b
siobopfadzani <pfadzani@gmail.com>
2023-05-08 14:27:45 +0200

    commit push

--- fundamentals/9_numerictypes/FloatTest3.java
+++ fundamentals/9_numerictypes/FloatTest3.java
@@ -1,8 +1,8 @@
 public class CharTest{
     public static void main(String [] args){
-	    char myChar = 1 + '4';
+	    long mylong = 1 + '4';
 		
-		System.out.println(myChar);
+		System.out.println(mylong);
 	}
 }
 

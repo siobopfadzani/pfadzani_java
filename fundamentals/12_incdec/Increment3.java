402449b70fc1d971585f18515705868ad57d6bf2
siobopfadzani <pfadzani@gmail.com>
2023-05-09 14:45:44 +0200

    commit push

---
+++ fundamentals/12_incdec/Increment3.java
@@ -0,0 +1,10 @@
+public class Increment3 {
+    public static void main(String[] args) {
+ 	   int myInt = 2;
+ 	   System.out.println("Before operation myInt = "+myInt);
+ 	   System.out.println("During operation myInt = "+ (myInt--));
+ 	   System.out.println("After operation myInt = "+myInt); 		   
+    }
+}
+
+
---
+++ fundamentals/12_incdec/Increment4.java
@@ -0,0 +1,10 @@
+public class Increment4 {
+    public static void main(String[] args) {
+ 	   int myInt = 2;
+ 	   System.out.println("Before operation myInt = "+myInt);
+ 	   System.out.println("During operation myInt = "+ (--myInt));
+ 	   System.out.println("After operation myInt = "+myInt); 		   
+    }
+}
+
+

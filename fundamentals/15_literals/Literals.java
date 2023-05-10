139b588feb3a8cb5a1b68867ab56d13ae576320c
siobopfadzani <pfadzani@gmail.com>
2023-05-10 12:41:26 +0200

    commit push

---
+++ fundamentals/15_literals/Literals.java
@@ -0,0 +1,9 @@
+public class Literals1{
+public static void main(String [] arg){
+int a =100;
+int b = 200;
+int sum = a+b;
+System.out.println("The sum of my operation is :"+ sum);
+
+  }
+}
---
+++ fundamentals/15_literals/literals1.java
@@ -0,0 +1,7 @@
+public class HexadecimalLiterals{
+public static void main(String [] arg){
+int num1 = 0xA;
+int num2 = 0xB;
+System.out.println("multiplied hexadecemal value: " + num1*num2);
+  }
+}

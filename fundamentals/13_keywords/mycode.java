242c862772db025d25852b9d88f47844c6c12855
siobopfadzani <pfadzani@gmail.com>
2023-05-10 05:34:31 +0200

    commit push

---
+++ fundamentals/13_keywords/mycode.java
@@ -0,0 +1,15 @@
+public class mypracticecode {
+public static void main(String [] arg){
+int a = 10;
+int b =20;
+int c = 30;
+int total =b/a;
+int number = c*b/a;
+String name = "hello";
+System.out.println("total is:"+" "+ total);
+System.out.println("total plus one is:"+ ++total);
+System.out.println("total minus "+ --total);
+System.out.println ("my variable totals test:" + number);
+}
+
+}

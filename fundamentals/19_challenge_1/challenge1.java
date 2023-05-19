f0c31c6e1f69c990498e41125ff9fbab17956797
siobopfadzani <pfadzani@gmail.com>
2023-05-19 08:37:01 +0200

    commit push

---
+++ fundamentals/19_challenge_1/challenge1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class PromptUserDetails{
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("What is youe name :");
+        String name = scan.nextLine();
+        
+        System.out.println("What is your surname");
+        String surname =scan.nextLine();
+        
+        System.out.println("Enter your date of birth (DD/MM/YYYY):");
+        String date_of_birth = scan.nextLine();
+        
+        System.out.println("How far is favourite store from home");
+        String distance_from_store = scan.nextLine();
+        
+        System.out.println("hello : "+ name +" " +surname);
+        
+        String year = date_of_birth.split("/")[2];
+        
+        int age = 2023 - Integer.parseInt(year);
+        System.out.println("you are : "+ age +" years old ");
+        
+        int meters =1000;
+        double miles =0.621371;
+        double seconds = 0.277777778;
+        //long miliseconds = 31556926000;
+        
+        System.out.println("Distance in Mile: " +  miles*Integer.parseInt(distance_from_store)+ "mil." );
+       
+        System.out.println("Distance in meters: " +  meters*Integer.parseInt(distance_from_store)+ "m" );
+        System.out.println("Age in seconds: " +  seconds*Integer.parseInt(year)+ "s" );
+        
+       // System.out.println("Age in miliseconds:" +  miles*Integer.parseInt(year)+ "mi." );
+    }
+}
+
+

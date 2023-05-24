75b8efbf1606dcdbd7e424907de1796df52feed0
siobopfadzani <pfadzani@gmail.com>
2023-05-24 15:19:12 +0200

    commit push

---
+++ fundamentals/20_if/IfTest2variation3.java
@@ -0,0 +1,14 @@
+public class IfTest2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+       
+        if(a < b){
+            System.out.println("COOL");
+        }
+       
+        System.out.println("Finished");
+    }
+}
+
+
---
+++ fundamentals/20_if/IfTest5.java
@@ -0,0 +1,14 @@
+public class IfTest5 {
+    public static void main(String[] args) {
+        boolean test = false;
+        if(test){
+            System.out.println("COOL");
+        }
+        else{
+            System.out.println("NOT COOL");
+        }
+    }
+}
+
+
+

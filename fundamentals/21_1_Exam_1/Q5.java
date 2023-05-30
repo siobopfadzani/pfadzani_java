6b43671a82e1f11510ce886fc18b86c824e56b74
siobopfadzani <pfadzani@gmail.com>
2023-05-30 12:11:45 +0200

    commit push

--- fundamentals/21_1_Exam_1/Q5.java
+++ fundamentals/21_1_Exam_1/Q5.java
@@ -2,7 +2,7 @@ public class Test{
 	public static void main(String[]args){
 	
 	int a = 2000;
-	String results=((a%4==0)&&(a%100==0)&&(a%400==0)?(a%400==0)? "first":"second":"third");
+	String results=((a%4==0)&&(a%100!=0)&&(a%400==0)?(a%400==0)? "first":"second":"third");
 	System.out.println(results);
 	
 	}

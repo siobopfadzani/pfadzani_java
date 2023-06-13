6bfc1bc870d64034dc1ebf563f376090e5b425f3
siobopfadzani <pfadzani@gmail.com>
2023-06-13 14:40:14 +0200

    commit push

---
+++ fundamentals/32_while/WhileTestvariation2.java
@@ -0,0 +1,12 @@
+public class WhileTest1 {
+    public static void main(String[] args) {
+        int [] lottonumbers = {233,444,678,675,897,678,453,567,455,456,453};
+       int i=-1;
+        while(i++ < 10){
+            System.out.println("index = "+ i +" value = "+lottonumbers[i]);
+        }
+    }
+}
+
+
+

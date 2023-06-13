7cbcdc761b19b818730f3aec5c5e6e5c8925ac35
siobopfadzani <pfadzani@gmail.com>
2023-06-13 11:20:16 +0200

    commit push

--- fundamentals/31_nestfor/NestedForvariation2.java
+++ fundamentals/31_nestfor/NestedForvariation2.java
@@ -1,7 +1,7 @@
 public class Practice {
     
     public Practice() {
-        for (int val = 0; val < 10; val++) {  
+        for (int val = 0; val < 10; val+=2) {  
             for (int val1 = 2; val1 < 10; val1 += 2) {
                 System.out.println("value1 = " + val + " value2 = " + val1);
             }
@@ -9,16 +9,15 @@ public class Practice {
     }
     
     public void Practice1() {
-        for (int a = 0; a < 2; a++) {
+        for (int a = 0; a < 5; a++) {
             System.out.println("value for my test is " + a);
         }
     }
     
     public static void main(String[] args) {
         Practice object = new Practice();
-        Practice object1 = new Practice();
         object.Practice1();
-        //object1.Practice1();
+        
         
     }
 }

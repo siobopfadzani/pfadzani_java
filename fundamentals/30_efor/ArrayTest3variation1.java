24974f8bac8ad005412c1a571459b2ee74b8b04b
siobopfadzani <pfadzani@gmail.com>
2023-06-13 08:55:40 +0200

    commit push

---
+++ fundamentals/30_efor/ArrayTest3variation1.java
@@ -0,0 +1,17 @@
+public class ArrayTest3 {
+    public static void main(String[] args) {
+        int [] lottonumbers = {233,444,678,675,897,678,453,567,455,456,453};
+       
+        int i = 1;       
+
+        for(int tempValue : lottonumbers){
+        if (i%2 !=0){
+            System.out.println("index = "+ i +" value = "+tempValue);
+            
+          
+            }
+            i++;
+        }
+    }
+}
+
---
+++ fundamentals/30_efor/ArrayTest3variation2.java
@@ -0,0 +1,22 @@
+
+
+public class test{
+    public test(){
+    int [] array1 = {10,20,20,40,50,60,70,80,90,100,110};
+    
+    int a =1;
+    
+    for (int num:array1){
+       if (a>=0){
+       System.out.println(a + " "+ num);
+       }
+       a++;
+    }
+
+}
+public static void main(String[]args){
+   
+    test  Object = new test();
+  }
+}
+

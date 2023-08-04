d1bb494dc3d799757bf72ad7f7fb536baf3c357b
siobopfadzani <pfadzani@gmail.com>
2023-08-04 09:17:49 +0200

    push commit

---
+++ fundamentals/23_arrays_exam_7/Q11.java
@@ -0,0 +1,12 @@
+public class Q11{
+   
+   public static void main(String[]args)
+   {
+      int i;
+      int j;
+      for(i =0,j=0;j<i;--j,i--){
+         System.out.println(i +"star" +j);
+   }
+   System.out.println(i + "hello"+j); 
+   }
+}
---
+++ fundamentals/23_arrays_exam_7/Q14.java
@@ -0,0 +1,18 @@
+public class Q14{
+   
+   public static void main(String[]args)
+   {
+     int num = 120;
+     final int num2
+     num2= 20;
+     switch(num)
+     {
+        default:
+        System.out.println("default");
+        case num2:
+          System.out.println("2");
+          break;
+     }
+   }
+
+}

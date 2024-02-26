473442646a9a96145cf18d9e42bae363406cddd6
pfadzani-siobo <pfadzani@gmail.com>
2024-02-26 15:13:43 +0200

    comit and push

--- OOP/7.0.4 Encapsulation/3/MorphMain.java
+++ OOP/7.0.4 Encapsulation/3/MorphMain.java
@@ -1,5 +1,10 @@
 public class MorphMain
 {
-    Morph1 morph1 = new Morph1();
-    morph1
+    public static void main(String[]args)
+    {
+        Morph1 morph1 = new Morph1();
+        Object target;
+        target = morph1;;
+    }
+
 }

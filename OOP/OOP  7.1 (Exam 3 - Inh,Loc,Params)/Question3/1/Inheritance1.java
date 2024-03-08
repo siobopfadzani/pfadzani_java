8a2972f2942a90f80a055e5393cbdabbbf45047a
pfadzani-siobo <pfadzani@gmail.com>
2024-03-08 09:58:16 +0200

    Commit and push

---
+++ OOP/OOP  7.1 (Exam 3 - Inh,Loc,Params)/Question3/1/Inheritance1.java
@@ -0,0 +1,12 @@
+public class Inheritance1
+{
+    public static void main(String[] args) 
+    {
+        Inheritance2 inherit = new Inheritance2();
+        inherit.test();    
+    }
+    public void test()
+    {
+        System.out.println("test");
+    }
+}
---
+++ OOP/OOP  7.1 (Exam 3 - Inh,Loc,Params)/Question3/5/Inheritance2.java
@@ -0,0 +1,8 @@
+
+public class Inheritance2 extends Inheritance1
+{
+    public void test()
+    {
+        System.out.println("test");
+    }
+}

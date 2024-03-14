e9d46a07a1a927733443637b9e53a3e432ff064e
pfadzani-siobo <pfadzani@gmail.com>
2024-03-14 10:54:46 +0200

    commit and push

---
+++ src/Country.java
@@ -0,0 +1,29 @@
+public class Country
+{
+    static int coutryCounter;
+    String name;
+    static String name1;
+    int dummyCounter;
+
+    public static void main(String[]args)
+    {
+        Country ob1 = new Country();
+
+        ob1.name = "name";
+        Country.name1 ="name1";
+        ob1.dummyCounter++;
+
+        Country ob2 = new Country();
+
+        ob2.dummyCounter++;
+        ob2.name = "obj2  name";
+        ob2.name1 = "obj2 name1";
+        Country.coutryCounter++;
+
+        System.out.println(name1);
+        System.out.println(ob1.name);
+
+
+
+    }
+}
